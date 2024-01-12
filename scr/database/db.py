84bab55282e045e119306d148a84be2a5b857094
OleksaSolo <131910119+OleksaSolo@users.noreply.github.com>
2024-01-12 13:15:50 +0100

    Update db.py

--- scr/database/db.py
+++ scr/database/db.py
@@ -2,7 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:567234@localhost:5432/postgres"
-# SQLALCHEMY_DATABASE_URL = "postgres://koyeb-adm:XP7mswbKhYU8@ep-broad-dew-69547104.eu-central-1.pg.koyeb.app/koyebdb"
 #SQLALCHEMY_DATABASE_URL = "postgresql+asyncpg2://postgres:567234@localhost:5432/postgres"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
